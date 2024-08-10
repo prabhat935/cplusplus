853e8f6d595d58b345a2a2a41ededea84f7414b0
Prabhat Chaturvedi <prabhat.a.chaturvedi@capgemini.com>
2024-08-10 10:03:41 +0530

    Intro to c++

---
+++ IntroductionToCplusplus.cpp
@@ -0,0 +1,18 @@
+/****************************Notes****************************************/
+//Object Oriented Programming
+    // Abstraction
+    // Encapsulation
+    // Inheritance
+    // Polymorphism
+//Not Pure OOPs Why??
+//Extension To C++
+//Bjarne Stroustrup
+//Developed In BEll Labs
+//Started development in 1979
+//First Release in 1983
+//Previously known as"C with Classes" later changed to c++
+//C++ is a
+    //statically typed
+    //free form
+    //compiled
+    //general purpose language
