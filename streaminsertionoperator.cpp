0c029bd600fffb8bac770dad2e838fc09159ce73
Prabhat Chaturvedi <kumarprabhat707@gmail.com>
2024-09-25 21:26:39 -0700

    stream insertion and extraction

---
+++ streaminsertionoperator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Test{
+    int x;
+public:
+    Test(int a = 0):x{a}{}
+    friend istream& operator>>(istream& input , Test& obj);
+    friend ostream& operator<<(ostream& output , Test& obj);
+};
+
+istream& operator>>(istream& input , Test& obj){
+    input >> obj.x;
+    return input;
+}
+
+ostream& operator<<(ostream& output , Test& obj){
+    output << obj.x << endl;
+    return output;
+}
+
+int main(){
+    Test t;
+    cin >> t;
+    cout << t;
+    return 0;
+}
